ad1109c0aada78afde8030bc229a41297861f243
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-21 16:23:17 -0300

    Create routes and controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,33 @@
+const Category = require("../models/category");
+
+exports.category_list = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.category_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
---
+++ controllers/itemController.js
@@ -0,0 +1,37 @@
+const Item = require("../models/item");
+
+exports.index = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_list = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
+
+exports.item_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED YET");
+};
